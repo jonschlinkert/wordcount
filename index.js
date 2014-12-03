5320ca11c36490dcfe896eb75f7186855d4071b5
jonschlinkert <dev@sellside.com>
2014-12-03 00:10:08 -0500

    change strategy

--- index.js
+++ index.js
@@ -7,12 +7,14 @@
 
 'use strict';
 
-
-// Split the text into an array of words
-var wordArray = function (str) {
-  return str.split(/[\s]+/).filter(Boolean);
-};
-
 module.exports = function wordcount(str) {
-  return wordArray(str).length;
+  // regex from https://github.com/lepture/editor/blob/master/src/intro.js#L343
+  var pattern = /[a-zA-Z0-9_\u0392-\u03c9]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af]+/g;
+
+  var matches = str.match(pattern);
+  if (matches === null) {
+    return 0;
+  }
+
+  return matches.length;
 };
