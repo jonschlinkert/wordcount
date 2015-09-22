d03da27684f0dd52dff737ffe82cab98b8099bcb
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-22 05:43:25 -0400

    externalize regex

--- index.js
+++ index.js
@@ -7,12 +7,12 @@
 
 'use strict';
 
-// Modified from: https://github.com/lepture/editor/blob/master/src/intro.js#L343
-var regex = function() {
-  return /[a-zA-Z0-9_\u0392-\u03c9\u0400-\u04FF]+|[\u4E00-\u9FFF\u3400-\u4dbf\uf900-\ufaff\u3040-\u309f\uac00-\ud7af\u0400-\u04FF]+|\w+/g;
-};
+var regex = require('word-regex');
 
 module.exports = function wordcount(str) {
+  if (typeof str !== 'string') {
+    throw new TypeError('expected a string');
+  }
   var matches = str.match(regex());
   return matches !== null
     ? matches.length
