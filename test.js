a625a96a377f460331d47f66a9c5ca06e599c4ba
jonschlinkert <jon.schlinkert@sellside.com>
2015-09-22 05:43:18 -0400

    lint

--- test.js
+++ test.js
@@ -1,31 +1,26 @@
-/*!
- * wordcount <https://github.com/jonschlinkert/wordcount>
- *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT License
- */
-
-/* deps: mocha */
+require('mocha');
 var assert = require('assert');
 var wordcount = require('./');
 
-it('should count the words in a string.', function () {
-  assert.strictEqual(wordcount('Count the words in string.'), 5);
-  assert.strictEqual(wordcount('Count the words in string, again.'), 6);
-});
+describe('count words', function () {
+  it('should count the words in a string.', function () {
+    assert.strictEqual(wordcount('Count the words in string.'), 5);
+    assert.strictEqual(wordcount('Count the words in string, again.'), 6);
+  });
 
-it('should count the words in a cyrillic string.', function () {
-  assert.strictEqual(wordcount('Тест стринг кирилица.'), 3)
-});
+  it('should count the words in a cyrillic string.', function () {
+    assert.strictEqual(wordcount('Тест стринг кирилица.'), 3)
+  });
 
-it('should count the words in mixed latin and cyrillic string', function () {
-  assert.strictEqual(wordcount('Тест mixed стринг кирилица and latin string.'), 7)
-});
+  it('should count the words in mixed latin and cyrillic string', function () {
+    assert.strictEqual(wordcount('Тест mixed стринг кирилица and latin string.'), 7)
+  });
 
-it('should count the words in mixed chinese (traditional) and latin string', function () {
-  assert.strictEqual(wordcount('We are 我們是 朋友 from Bulgaria'), 6);
-});
+  it('should count the words in mixed chinese (traditional) and latin string', function () {
+    assert.strictEqual(wordcount('We are 我們是 朋友 from Bulgaria'), 6);
+  });
 
-it('should count the words in korean-latin-cyrillic string', function () {
-  assert.strictEqual(wordcount('I am from България, and speak 한국어 언어'), 8);
+  it('should count the words in korean-latin-cyrillic string', function () {
+    assert.strictEqual(wordcount('I am from България, and speak 한국어 언어'), 8);
+  });
 });
